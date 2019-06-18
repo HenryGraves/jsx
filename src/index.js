c84f07a6f8bfa8ab226845e9fe1b7ee4e92154d9
Henry <henrygraves95@gmail.com>
2019-06-18 01:11:57 -0700

    react jsx tutorial complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// import react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+const App = () => {
+  const buttonText = { text: 'click me!'};
+  const labelText = 'enter name';
+  return (
+    <div>
+      <label className="label" for="name">
+         {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+}
+// render react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
